bae51ef8867053098d80f783b706e412f777ddb1
Thiago Gaspar <thiagogaspar@MacBook-Air-de-Thiago.local>
2021-08-29 20:50:20 -0300

    teste

--- jogoDaVelha/Views/HomeView.swift
+++ jogoDaVelha/Views/HomeView.swift
@@ -15,11 +15,11 @@ class HomeView: UIView {
      **  MARK: Variables Declaration
      **
      **********************************************************************************/
-
+    
     var ballImageView : UIImageView!
     
     var playImageView : UIImageView!
-
+    
     var playLabel : UILabel!
     
     var crossImageView : UIImageView!
@@ -37,19 +37,19 @@ class HomeView: UIView {
     var secondGameView : UIView!
     
     var thirdGameView : UIView!
-
+    
     var fourthGameView : UIView!
-
+    
     var fifthGameView : UIView!
-
+    
     var sixthGameView : UIView!
-
+    
     var seventhGameView : UIView!
-
+    
     var eightGameView : UIView!
-
+    
     var nineGameView : UIView!
-
+    
     
     /* ******************************************************************************
      **
@@ -64,10 +64,27 @@ class HomeView: UIView {
         
         let width = view.frame.size.width
         let height = view.frame.size.height
-               
-        //-------------------------  -----------------------------
-    
-       
+        
+        var yPosition = height*0.05
+        
+        //------------------------- Ball Image View -----------------------------
+        
+        ballImageView = UIImageView(frame: CGRect(x: width*0.1, y: yPosition, width: 40, height: 40))
+        ballImageView.image = UIImage(named: "ball")
+        ballImageView.contentMode = .scaleAspectFit
+        ballImageView.layer.cornerRadius = ballImageView.frame.height/2
+        
+        view.addSubview(ballImageView)
+        
+        //------------------------- Play Image View -----------------------------
+
+        playImageView = UIImageView(frame: CGRect(x: 0, y: yPosition, width: 100, height: 40))
+        playImageView.image = UIImage(named: "ball")
+        playImageView.contentMode = .scaleAspectFit
+        playImageView.center.x = width/2
+        
+        view.addSubview(playImageView)
+        
         
         
     }
